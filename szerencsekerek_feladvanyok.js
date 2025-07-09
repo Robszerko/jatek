bcc02857e77f48ca7af6dedf38743821397ce516
Robszerko <robszerko@gmail.com>
2025-07-09 21:35:56 +0200

    Add files via upload

---
+++ szerencsekerek_feladvanyok.js
@@ -0,0 +1,24 @@
+// Fájl: szerencsekerek_feladvanyok.js
+
+const wheelPuzzles = [
+    { category: "Közmondás", puzzle: "ADDIG JÁR A KORSÓ A KÚTRA AMÍG EL NEM TÖRIK" },
+    { category: "Film & Rendező", puzzle: "QUENTIN TARANTINO PULP FICTION" },
+    { category: "Élőlény", puzzle: "AZ EMBERI TEST LEGKISEBB CSILLÁMA" },
+    { category: "Tárgy", puzzle: "AUTOMATA NARANCSFACSARÓ GÉP" },
+    { category: "Fogalom", puzzle: "AZ IDŐ MINDENT MEGOLD" },
+    { category: "Étel & Ital", puzzle: "BÉCSI SZELET KRUMPLISALÁTÁVAL" },
+    { category: "Zene", puzzle: "QUEEN BOHEMIAN RHAPSODY" },
+    { category: "Híres Ember", puzzle: "MUNKÁCSY MIHÁLY" },
+    { category: "Földrajz", puzzle: "A MARIANNA ÁROK MÉLYSÉGE" },
+    { category: "Történelem", puzzle: "A MÁSODIK VILÁGHÁBORÚ VÉGE" },
+    { category: "Irodalom", puzzle: "JÓKAI MÓR AZ ARANY EMBER" },
+    { category: "Sport", puzzle: "PAPP LÁSZLÓ HÁROMSZOROS OLIMPIAI BAJNOK" },
+    { category: "Tudomány", puzzle: "ALBERT EINSTEIN RELATIVITÁSELMÉLETE" },
+    { category: "Márkanév", puzzle: "MERCEDES BENZ" },
+    { category: "Mindennapi élet", puzzle: "A BLUETOOTH CSATLAKOZTATÁSA SIKERTELEN" },
+    { category: "Növény", puzzle: "JAPÁN CSERESZNYEVIRÁGZÁS" },
+    { category: "Dalszöveg", puzzle: "AZÉRT VANNAK A JÓBARÁTOK" },
+    { category: "Tárgy", puzzle: "ZSEBKÉS" },
+    { category: "Fogalom", puzzle: "VÉGTELEN" },
+    { category: "Élőlény", puzzle: "KIRÁLYPINGVIN" }
+];
